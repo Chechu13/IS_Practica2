40bbe619241ca65f51876f2bf7d9f7c527e0dc7b
Chechu13 <150666817+Chechu13@users.noreply.github.com>
2025-12-18 19:05:37 +0100

    Fatal Error Solved

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ sistema = Controlador()
 print("--- SERVIDOR WEB INICIANDO ---")
 sistema.cargar_datos()
 
-generator = np.random.default_rng(42)
+generator = 42
 df_train, df_test = train_test_split(sistema.df, test_size=0.20, shuffle=False, random_state=generator)
 sistema.detector.entrenar(df_train, df_test)
 lista_incidencias_cache = sistema.detector.detectar_incidencias(df_test)
